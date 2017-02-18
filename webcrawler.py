ba1b7e4fc6cbb0f534179cfffed2d37dfa263706
Diego Milan <diego.088@gmail.com>
2017-02-18 23:24:43 +0000

    Crawl sites and wait for all threads

--- webcrawler.py
+++ webcrawler.py
@@ -3,6 +3,7 @@ import logging
 import sys
 
 import eventlet
+from eventlet.green import urllib2
 import validators
 
 
@@ -15,11 +16,11 @@ args = parser.parse_args()
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 
-def ensure_url_protocol(url):
+def ensure_protocol_in_url(url):
     """If protocol not provided fallback to http"""
     if not (url.startswith('https://') or url.startswith('http://')):
         logging.debug("Protocol not provided. Falling back to http.")
-        url = "http://" + url
+        return "http://" + url
     return url
 
 
@@ -33,17 +34,21 @@ def validate_url(url):
 def crawl(url, visited, pool):
     """Crawl url, build site's map and list its assets"""
     logging.debug("Crawling {}".format(url))
+    with eventlet.Timeout(5, False):
+        data = urllib2.urlopen(url).read()
+        logging.debug(data)
 
 
 
 if __name__ == '__main__':
     # Get url and validate
-    max_threads = args.max_threads
     url = args.url
-    url = ensure_url_protocol(url)
+    max_threads = args.max_threads
+    url = ensure_protocol_in_url(url)
     validate_url(url)
 
     # Perform crawling
     visited = set()
     pool = eventlet.GreenPool(size=max_threads)
     crawl(url, visited, pool)
+    pool.waitall()
