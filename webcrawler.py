f0aad5ef3fe79766f1c100d08e8f36de0264d904
Diego Milan <diego.088@gmail.com>
2017-02-18 20:53:41 +0000

    Ensure protocol in url

--- webcrawler.py
+++ webcrawler.py
@@ -13,17 +13,28 @@ args = parser.parse_args()
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 
+def ensure_protocol(url):
+    """If protocol not provided fallback to http"""
+    if not (url.startswith('https://') or url.startswith('http://')):
+        logging.debug("Protocol not provided. Falling back to http.")
+        url = "http://" + url
+    return url
+
+
 def validate_url(url):
+    """Check if url is valid, exit otherwise"""
     if not validators.url(url):
         logging.error("URL provided ({}) is not valid.".format(url))
         exit(1)
 
 
 def crawl(url):
-    pass
+    """Crawl url, build site's map and list its assets"""
+    logging.debug("Crawling {}".format(url))
 
 
 if __name__ == '__main__':
     url = args.url
+    url = ensure_protocol(url)
     validate_url(url)
     crawl(url)
