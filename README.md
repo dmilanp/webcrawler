8541637923fb0b8379053f4057d07ee4ed1a1bc7
Diego Milan <diego.088@gmail.com>
2017-02-18 20:33:08 +0000

    Validate url provided by user

---
+++ README.md
@@ -0,0 +1,21 @@
+# WebCrawler
+
+## Installation
+
+```
+virtualenv env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+## Usage
+
+```
+python webcrawler [URL] 
+```
+
+## Help
+
+```
+python webcrawler -h
+```
--- webcrawler.py
+++ webcrawler.py
@@ -1,15 +1,27 @@
 import argparse
+import logging
+import sys
 
-parser = argparse.ArgumentParser(description='This script takes a url to crawl and outputs its site\'s map and a full'
+import validators
+
+
+parser = argparse.ArgumentParser(description='This script takes a url to crawl and outputs its site\'s map and a full '
                                              'list of its assets.')
 parser.add_argument('url', type=str, help='url to crawl')
 args = parser.parse_args()
 
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+
 def validate_url(url):
-    pass
+    if not validators.url(url):
+        logging.error("URL provided ({}) is not valid.".format(url))
+        exit(1)
+
 
 def crawl(url):
     pass
+
 
 if __name__ == '__main__':
     url = args.url
