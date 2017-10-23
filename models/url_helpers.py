6e8afad825d0ff64ba1684941cdb379a0069378c
Diego Milan <diego.088@gmail.com>
2017-10-23 23:39:09 +0100

    Improves helper functions

--- models/url_helpers.py
+++ models/url_helpers.py
@@ -5,75 +5,71 @@ import tldextract
 import validators
 
 
-def domain_for_url(url):
-    tld = tldextract.extract(url)
+def top_level_domain(url):
+    # type: (str) -> str
+    _url = ensure_http_scheme(url)
+    tld = tldextract.extract(_url)
     return "{}.{}".format(tld.domain, tld.suffix)
 
 
+def url_path(url):
+    _url = ensure_http_scheme(url)
+    return urlparse(_url).path
+
+
 def is_valid_url(url):
-    """Checks if url is valid"""
-    if not validators.url(ensure_url_protocol(url)):
-        return False
-    return True
+    # type: (str) -> bool
+    """Checks if url is valid. Requires scheme in url."""
+    _url = ensure_http_scheme(url)
+    if validators.url(_url):
+        return True
+    return False
 
 
-def ensure_url_protocol(url):
-    """If protocol not provided fallback to http"""
+def ensure_http_scheme(url):
+    # type: (str) -> str
     if not url:
         return None
-
-    if not (url.startswith('https://') or url.startswith('http://')):
+    elif url.startswith('http'):
+        return url
+    else:
         return "http://" + url
-
-    return url
-
-
-def extract_path_from_url(url):
-    """Returns resource part of url without domain"""
-    return urlparse(ensure_url_protocol(url)).path
 
 
 def link_from_domain_or_none(link, domain):
     """If link is to same domain this returns it well formatted, otherwise returns None"""
-    if (not link or
-            'mailto:' in link or
-            'tel:' in link):
+    if link is None or 'mailto:' in link or 'tel:' in link:
         return None
 
-    if domain not in urlparse(link).netloc:
+    tld = top_level_domain(domain)
+
+    if tld not in urlparse(ensure_http_scheme(link)).netloc:
         # Guard cases like https://plus.google.com/share?url=http%3A%2F%2Fwww.headspace.com
         return None
-
-    if domain in link:
-        link = ensure_url_protocol(link)
-        link_with_clean_slashes = re.sub('http://[/]+', 'http://', link)
-        return link_with_clean_slashes
     else:
-        # Relative link
-        link = link.replace('../', '')
-        link = re.sub('^/', '', link)
-        return ensure_url_protocol(
-            urlparse(domain).netloc + '/' + link
+        _link = re.sub('[\./]+', '', link)
+        return ensure_http_scheme(
+            urlparse(domain).netloc + '/' + _link
         )
 
 
-def is_asset(tag):
+def is_relevant_asset(tag):
     return tag.name in ['a', 'link', 'img', 'script']
 
 
-def get_asset_link(tag):
+def get_link_from_asset(tag):
     if tag.name == 'a' or tag.name == 'link':
         return tag.get('href')
     elif tag.name == 'img' or tag.name == 'script':
         return tag.get('src')
 
 
-def prepare_asset(asset, url):
-    parsed_url = urlparse(url)
-    if asset.startswith('www'):
-        return ensure_url_protocol(asset)
-    elif asset.startswith('http'):
-        return asset
-    elif asset.startswith('/'):
+def build_asset_url(asset, url):
+    _url = ensure_http_scheme(url)
+    parsed_url = urlparse(_url)
+
+    if asset.startswith('/'):
         asset = re.sub('^/', '', asset)
         return '{}/{}'.format(parsed_url.netloc, asset)
+
+    return ensure_http_scheme(asset)
